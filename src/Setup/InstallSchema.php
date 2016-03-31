1cdfd3846a7030308d246a17710b11755676e167
Alex Gusev <alex@flancer64.com>
2016-03-31 14:07:51 +0300

    MOBI-265 - scheme setup migration

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -7,12 +7,31 @@ namespace Praxigento\Pv\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Praxigento\Core\Lib\Setup\Db as Db;
+use Praxigento\Pv\Lib\Entity\Sale;
+use Praxigento\Pv\Lib\Entity\Sale\Item as SaleItem;
 
 class InstallSchema extends \Praxigento\Core\Setup\Schema\Base
 {
     protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        // TODO: Implement _setup() method.
+        /**
+         * Read and parse JSON schema.
+         */
+        $pathToFile = __DIR__ . '/../etc/dem.json';
+        $pathToNode = '/dBEAR/package/Praxigento/package/Pv';
+        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+
+        /* Sale */
+        $entityAlias = Sale::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['SaleOrder'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Sale Item*/
+        $entityAlias = SaleItem::ENTITY_NAME;
+        $demEntity = $demPackage['package']['SaleOrder']['entity']['OrderItem'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
     }
 
 
