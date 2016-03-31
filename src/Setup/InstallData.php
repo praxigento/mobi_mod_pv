cd990c6ed4ebd29ac555d8182c81812d7a5ecda8
Alex Gusev <alex@flancer64.com>
2016-03-31 15:08:32 +0300

    MOBI-265 - data setup migration

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -8,13 +8,41 @@ namespace Praxigento\Pv\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Praxigento\Accounting\Lib\Entity\Type\Asset as TypeAsset;
+use Praxigento\Accounting\Lib\Entity\Type\Operation as TypeOperation;
+use Praxigento\Pv\Lib\Config as Cfg;
 
 class InstallData extends \Praxigento\Core\Setup\Data\Base
 {
-    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    private function _addAccountingAssetsTypes()
     {
-        // TODO: Implement _setup() method.
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeAsset::ENTITY_NAME),
+            [TypeAsset::ATTR_CODE, TypeAsset::ATTR_NOTE],
+            [
+                [
+                    Cfg::CODE_TYPE_ASSET_PV,
+                    'PV (Points of volume or volume points).'
+                ]
+            ]
+        );
     }
 
+    private function _addAccountingOperationsTypes()
+    {
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeOperation::ENTITY_NAME),
+            [TypeOperation::ATTR_CODE, TypeOperation::ATTR_NOTE],
+            [
+                [Cfg::CODE_TYPE_OPER_PV_SALE_PAID, 'PV assets related to order processing.'],
+                [Cfg::CODE_TYPE_OPER_PV_TRANSFER, 'PV transfer between customers accounts.']
+            ]
+        );
+    }
 
+    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->_addAccountingAssetsTypes();
+        $this->_addAccountingOperationsTypes();
+    }
 }
