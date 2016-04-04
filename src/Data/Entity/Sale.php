2a1b969eb5f3d5b9b65e797830696d2bfed07582
Alex Gusev <alex@flancer64.com>
2016-04-04 14:53:56 +0300

    MOBI-265 - data/entity in PV  module

--- src/Data/Entity/Sale.php
+++ src/Data/Entity/Sale.php
@@ -5,11 +5,110 @@
 
 namespace Praxigento\Pv\Data\Entity;
 
-class Sale {
+use Praxigento\Core\Data\Entity\Base as EntityBase;
+
+class Sale extends EntityBase
+{
     const ATTR_DATE_PAID = 'date_paid';
     const ATTR_DISCOUNT = 'discount';
     const ATTR_SALE_ID = 'sale_id';
     const ATTR_SUBTOTAL = 'subtotal';
     const ATTR_TOTAL = 'total';
     const ENTITY_NAME = 'prxgt_pv_sale';
+
+    /**
+     * @return string
+     */
+    public function getDatePaid()
+    {
+        $result = parent::getData(self::ATTR_DATE_PAID);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getDiscount()
+    {
+        $result = parent::getData(self::ATTR_DISCOUNT);
+        return $result;
+    }
+
+    public function getEntityName()
+    {
+        return self::ENTITY_NAME;
+    }
+
+    public function getPrimaryKeyAttrs()
+    {
+        return [self::ATTR_SALE_ID];
+    }
+
+    /**
+     * @return int
+     */
+    public function getSaleId()
+    {
+        $result = parent::getData(self::ATTR_SALE_ID);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getSubtotal()
+    {
+        $result = parent::getData(self::ATTR_SUBTOTAL);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getTotal()
+    {
+        $result = parent::getData(self::ATTR_TOTAL);
+        return $result;
+    }
+
+    /**
+     * @param string $data
+     */
+    public function setDatePaid($data)
+    {
+        parent::setData(self::ATTR_DATE_PAID, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setDiscount($data)
+    {
+        parent::setData(self::ATTR_DISCOUNT, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setSaleId($data)
+    {
+        parent::setData(self::ATTR_SALE_ID, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setSubtotal($data)
+    {
+        parent::setData(self::ATTR_SUBTOTAL, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setTotal($data)
+    {
+        parent::setData(self::ATTR_TOTAL, $data);
+    }
+
 }
--- src/Data/Entity/Sale/Item.php
+++ src/Data/Entity/Sale/Item.php
@@ -4,10 +4,91 @@
  */
 namespace Praxigento\Pv\Data\Entity\Sale;
 
-class Item {
+use Praxigento\Core\Data\Entity\Base as EntityBase;
+
+class Item extends EntityBase
+{
     const ATTR_DISCOUNT = 'discount';
     const ATTR_SALE_ITEM_ID = 'sale_item_id';
     const ATTR_SUBTOTAL = 'subtotal';
     const ATTR_TOTAL = 'total';
     const ENTITY_NAME = 'prxgt_pv_sale_item';
+
+    /**
+     * @return double
+     */
+    public function getDiscount()
+    {
+        $result = parent::getData(self::ATTR_DISCOUNT);
+        return $result;
+    }
+
+    public function getEntityName()
+    {
+        return self::ENTITY_NAME;
+    }
+
+    public function getPrimaryKeyAttrs()
+    {
+        return [self::ATTR_SALE_ITEM_ID];
+    }
+
+    /**
+     * @return int
+     */
+    public function getSaleItemId()
+    {
+        $result = parent::getData(self::ATTR_SALE_ITEM_ID);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getSubtotal()
+    {
+        $result = parent::getData(self::ATTR_SUBTOTAL);
+        return $result;
+    }
+
+    /**
+     * @return double
+     */
+    public function getTotal()
+    {
+        $result = parent::getData(self::ATTR_TOTAL);
+        return $result;
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setDiscount($data)
+    {
+        parent::setData(self::ATTR_DISCOUNT, $data);
+    }
+
+    /**
+     * @param int $data
+     */
+    public function setSaleItemId($data)
+    {
+        parent::setData(self::ATTR_SALE_ITEM_ID, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setSubtotal($data)
+    {
+        parent::setData(self::ATTR_SUBTOTAL, $data);
+    }
+
+    /**
+     * @param double $data
+     */
+    public function setTotal($data)
+    {
+        parent::setData(self::ATTR_TOTAL, $data);
+    }
 }
