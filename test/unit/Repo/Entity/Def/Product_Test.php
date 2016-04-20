5e8300779b69c9c03f78bd5d0bc15254f9b7032e
Alex Gusev <alex@flancer64.com>
2016-04-20 20:24:56 +0300

    MOBI-277 - Restore unit tests after M1 & M2 merge

--- test/unit/Repo/Entity/Def/Product_Test.php
+++ test/unit/Repo/Entity/Def/Product_Test.php
@@ -6,27 +6,17 @@ namespace Praxigento\Pv\Repo\Entity\Def;
 
 include_once(__DIR__ . '/../../../phpunit_bootstrap.php');
 
-class Product_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
+class Product_UnitTest extends \Praxigento\Core\Test\BaseRepoEntityCase
 {
-    /** @var  \Mockery\MockInterface */
-    private $mConn;
-    /** @var  \Mockery\MockInterface */
-    private $mRepoGeneric;
     /** @var  Product */
     private $obj;
 
     protected function setUp()
     {
         parent::setUp();
-        /* create mocks */
-        $this->mConn = $this->_mockConn();
-        $this->mRepoGeneric = $this->_mockRepoGeneric();
-        /* setup mocks for constructor */
-        // parent::__construct($resource);
-        $mResource = $this->_mockResourceConnection($this->mConn);
         /* create object to test */
         $this->obj = new Product(
-            $mResource,
+            $this->mResource,
             $this->mRepoGeneric
         );
     }
--- test/unit/Repo/Entity/Stock/Def/Item_Test.php
+++ test/unit/Repo/Entity/Stock/Def/Item_Test.php
@@ -6,27 +6,17 @@ namespace Praxigento\Pv\Repo\Entity\Stock\Def;
 
 include_once(__DIR__ . '/../../../../phpunit_bootstrap.php');
 
-class Item_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
+class Item_UnitTest extends \Praxigento\Core\Test\BaseRepoEntityCase
 {
-    /** @var  \Mockery\MockInterface */
-    private $mConn;
-    /** @var  \Mockery\MockInterface */
-    private $mRepoGeneric;
     /** @var  Item */
     private $obj;
 
     protected function setUp()
     {
         parent::setUp();
-        /* create mocks */
-        $this->mConn = $this->_mockConn();
-        $this->mRepoGeneric = $this->_mockRepoGeneric();
-        /* setup mocks for constructor */
-        // parent::__construct($resource);
-        $mResource = $this->_mockResourceConnection($this->mConn);
         /* create object to test */
         $this->obj = new Item(
-            $mResource,
+            $this->mResource,
             $this->mRepoGeneric
         );
     }
