ba81559697824908a94f39c81304a47bbf34b482
Alex Gusev <alex@flancer64.com>
2016-05-27 14:16:09 +0300

    MOBI-297 - Tests updates after qty register and wrhs detector (Odoo module)

--- src/Repo/Entity/IProduct.php
+++ src/Repo/Entity/IProduct.php
@@ -5,7 +5,43 @@
 
 namespace Praxigento\Pv\Repo\Entity;
 
+use Praxigento\Pv\Data\Entity\Product as Entity;
 
 interface IProduct extends \Praxigento\Core\Repo\IEntity
 {
-    }
+    /**
+     * @param array|Entity $data
+     * @return Entity
+     */
+    public function create($data);
+
+    /**
+     * Generic method to get data from repository.
+     *
+     * @param null $where
+     * @param null $order
+     * @param null $limit
+     * @param null $offset
+     * @param null $columns
+     * @param null $group
+     * @param null $having
+     * @return Entity[] Found data or empty array if no data found.
+     */
+    public function get(
+        $where = null,
+        $order = null,
+        $limit = null,
+        $offset = null,
+        $columns = null,
+        $group = null,
+        $having = null
+    );
+
+    /**
+     * Get the data instance by ID.
+     *
+     * @param int $id
+     * @return Entity|bool Found instance data or 'false'
+     */
+    public function getById($id);
+}
