7df9dbde30c955a679aa135acea41c4d989e5bbb
Alex Gusev <alex@flancer64.com>
2016-07-20 18:11:26 +0300

    MOBI-351 - Add PV columns to product grid and sale order grid

---
+++ src/Plugin/Framework/View/Element/UiComponent/DataProvider/CollectionFactory.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Pv\Plugin\Framework\View\Element\UiComponent\DataProvider;
+
+class CollectionFactory
+{
+    /** @var  Sub\QueryModifier */
+    protected $_subQueryModifier;
+
+    public function __construct(
+        Sub\QueryModifier $subQueryModufier
+    ) {
+        $this->_subQueryModifier = $subQueryModufier;
+    }
+
+    /**
+     * Modify result collection for "sales_order_grid_data_source" (add joins & filter mapping, MOBI-351).
+     *
+     * @param \Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory $subject
+     * @param \Closure $proceed
+     * @param $requestName
+     * @return null
+     */
+    public function aroundGetReport(
+        \Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory $subject,
+        \Closure $proceed,
+        $requestName
+    ) {
+        $result = $proceed($requestName);
+        if ($requestName == 'sales_order_grid_data_source') {
+            if ($result instanceof \Magento\Sales\Model\ResourceModel\Order\Grid\Collection) {
+                /* add JOINS to the select query */
+                $this->_subQueryModifier->populateSelect($result);
+                /* add fields to mapping */
+                $this->_subQueryModifier->addFieldsMapping($result);
+            }
+        }
+        return $result;
+    }
+}
---
+++ src/Plugin/Framework/View/Element/UiComponent/DataProvider/Sub/QueryModifier.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Pv\Plugin\Framework\View\Element\UiComponent\DataProvider\Sub;
+
+use Praxigento\Downline\Config as Cfg;
+use Praxigento\Pv\Data\Entity\Sale;
+
+class QueryModifier
+{
+    const AS_FLD_PV_DISCOUNT = 'prxgt_pv_discount';
+    const AS_FLD_PV_SUBTOTAL = 'prxgt_pv_subtotal';
+    const AS_FLD_PV_TOTAL = 'prxgt_pv_total';
+    const AS_TBL_PV_SALES = 'prxgtPvSales';
+
+    /** @var \Magento\Framework\App\ResourceConnection */
+    protected $_resource;
+
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resource
+    ) {
+        $this->_resource = $resource;
+    }
+
+    public function addFieldsMapping(
+        \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
+    ) {
+        // total
+        $fieldAlias = self::AS_FLD_PV_TOTAL;
+        $fieldFullName = self::AS_TBL_PV_SALES . '.' . Sale::ATTR_TOTAL;
+        $collection->addFilterToMap($fieldAlias, $fieldFullName);
+        // discount
+        $fieldAlias = self::AS_FLD_PV_DISCOUNT;
+        $fieldFullName = self::AS_TBL_PV_SALES . '.' . Sale::ATTR_DISCOUNT;
+        $collection->addFilterToMap($fieldAlias, $fieldFullName);
+        // subtotal
+        $fieldAlias = self::AS_FLD_PV_SUBTOTAL;
+        $fieldFullName = self::AS_TBL_PV_SALES . '.' . Sale::ATTR_SUBTOTAL;
+        $collection->addFilterToMap($fieldAlias, $fieldFullName);
+
+    }
+
+    public function populateSelect(
+        \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
+    ) {
+        $select = $collection->getSelect();
+        /* LEFT JOIN `prxgt_pv_sale` */
+        $tbl = [self::AS_TBL_PV_SALES => $this->_resource->getTableName(Sale::ENTITY_NAME)];
+        $on = self::AS_TBL_PV_SALES . '.' . Sale::ATTR_SALE_ID . '=main_table.' . Cfg::E_SALE_ORDER_A_ENTITY_ID;
+        $cols = [
+            self::AS_FLD_PV_TOTAL => Sale::ATTR_TOTAL,
+            self::AS_FLD_PV_DISCOUNT => Sale::ATTR_DISCOUNT,
+            self::AS_FLD_PV_SUBTOTAL => Sale::ATTR_SUBTOTAL
+        ];
+        $select->joinLeft($tbl, $on, $cols);
+        // $sql = (string)$query;
+        return $select;
+    }
+
+}
