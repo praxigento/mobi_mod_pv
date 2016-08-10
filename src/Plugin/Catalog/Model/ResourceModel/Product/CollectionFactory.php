52ec153859ce918a233dcae2cae42156efb89f15
Alex Gusev <alex@flancer64.com>
2016-08-10 14:50:23 +0300

    MOBI-393 - Restore Travis testing

--- src/Plugin/Catalog/Model/ResourceModel/Product/CollectionFactory.php
+++ src/Plugin/Catalog/Model/ResourceModel/Product/CollectionFactory.php
@@ -16,6 +16,14 @@ class CollectionFactory
     const AS_FLD_PV = 'prxgt_pv_wholesale';
     const AS_TBL_PROD_PV = 'prxgtPvProd';
     const FULL_PV = self::AS_TBL_PROD_PV . '.' . Product::ATTR_PV;
+    /** @var \Magento\Framework\App\ResourceConnection */
+    protected $_resource;
+
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resource
+    ) {
+        $this->_resource = $resource;
+    }
 
     public function aroundCreate(
         $subject,
@@ -26,15 +34,13 @@ class CollectionFactory
         if ($result instanceof \Magento\Catalog\Model\ResourceModel\Product\Collection) {
 
             $query = $result->getSelect();
-            $conn = $query->getConnection();
             /* LEFT JOIN `prxgt_pv_prod` AS `prxgtPvProd` */
-            $tbl = [self::AS_TBL_PROD_PV => $conn->getTableName(Product::ENTITY_NAME)];
+            $tbl = [self::AS_TBL_PROD_PV => $this->_resource->getTableName(Product::ENTITY_NAME)];
             $on = self::AS_TBL_PROD_PV . '.' . Product::ATTR_PROD_REF . '=e.entity_id';
             $cols = [
                 self::AS_FLD_PV => Product::ATTR_PV
             ];
             $query->joinLeft($tbl, $on, $cols);
-            $sql = (string)$query;
 
             /* add fields mapping */
             $result->addFilterToMap(self::AS_FLD_PV, self::FULL_PV);
