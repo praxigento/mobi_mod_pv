fc11e36f9d6f1fee5f969367da6938e9b68a9e88
Alex Gusev <alex@flancer64.com>
2017-12-21 15:30:30 +0200

    MOBI-1050 Display PV on checkout

--- src/view/frontend/web/js/view/checkout/cart/totals/subtotal.js
+++ src/view/frontend/web/js/view/checkout/cart/totals/subtotal.js
@@ -2,18 +2,49 @@
  *
  */
 define([
-    "Praxigento_Pv/js/view/checkout/cart/totals/base"
-], function (Component) {
+    "Praxigento_Pv/js/view/checkout/cart/totals/base",
+    "Magento_Checkout/js/model/totals"
+], function (Component, uiTotals) {
     "use strict";
+
+    /* save totals uiComponent to local context */
+    const totals = uiTotals;
 
     /* see \Praxigento\Pv\Plugin\Quote\Model\Cart\CartTotalRepository::SEGMENT_SUBTOTAL */
     const SEGMENT = 'prxgt_pv_subtotal';
+    /* see \Praxigento\Pv\Plugin\Quote\Model\Cart\CartTotalRepository::SEGMENT_GRAND*/
+    const SEGMENT_GRAND = 'prxgt_pv_grand';
+
+    /* pin prototype function to use in overridden 'isVisible' */
+    const fnIsVisible = Component.prototype.isVisible;
+
+    function getAmountGrand() {
+        var result = 0;
+        if (totals && totals.getSegment(SEGMENT_GRAND)) {
+            result = totals.getSegment(SEGMENT_GRAND).value;
+        }
+        return Number(result);
+    }
 
     var result = Component.extend({
         defaults: {
             title: "PV Subtotal",
             segmentName: SEGMENT
+        },
+
+        /**
+         * Extend visibility for summary node - hide if subtotal equals to grand.
+         *
+         * @returns {boolean}
+         */
+        isVisible: function () {
+            const parentResult = fnIsVisible.call(this);
+            const valueSub = this.getAmount();
+            const valueGrand = getAmountGrand();
+            const result = parentResult && (valueSub != valueGrand);
+            return result;
         }
     });
+
     return result;
 });
