e39d03d4b6d9c1a20807f084392ab0e0c619d93f
Alex Gusev <alex@flancer64.com>
2017-12-31 21:59:19 +0200

    MOBI-1073 Clean up InstallScheme classes in modules

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -15,36 +15,36 @@ use Praxigento\Pv\Repo\Entity\Data\Stock\Item as StockItem;
 class InstallSchema
     extends \Praxigento\Core\App\Setup\Schema\Base
 {
-    protected function _setup()
+    protected function setup()
     {
         /** Read and parse JSON schema. */
         $pathToFile = __DIR__ . '/../etc/dem.json';
         $pathToNode = '/dBEAR/package/Praxigento/package/Pv';
-        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+        $demPackage = $this->toolDem->readDemPackage($pathToFile, $pathToNode);
 
         /* Stock Item */
         $demEntity = $demPackage->get('package/Stock/entity/StockItem');
-        $this->_toolDem->createEntity(StockItem::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(StockItem::ENTITY_NAME, $demEntity);
 
         /* Product */
         $demEntity = $demPackage->get('entity/Product');
-        $this->_toolDem->createEntity(Product::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(Product::ENTITY_NAME, $demEntity);
 
         /* Sale */
         $demEntity = $demPackage->get('entity/SaleOrder');
-        $this->_toolDem->createEntity(Sale::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(Sale::ENTITY_NAME, $demEntity);
 
         /* Sale Item */
         $demEntity = $demPackage->get('package/SaleOrder/entity/OrderItem');
-        $this->_toolDem->createEntity(SaleItem::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(SaleItem::ENTITY_NAME, $demEntity);
 
         /* Quote */
         $demEntity = $demPackage->get('entity/SaleQuote');
-        $this->_toolDem->createEntity(Quote::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(Quote::ENTITY_NAME, $demEntity);
 
         /* Quote Item */
         $demEntity = $demPackage->get('package/SaleQuote/entity/QuoteItem');
-        $this->_toolDem->createEntity(QuoteItem::ENTITY_NAME, $demEntity);
+        $this->toolDem->createEntity(QuoteItem::ENTITY_NAME, $demEntity);
 
     }
 
