b60024a1087beb96a70928ec896f8435d38d48d7
Alex Gusev <alex@flancer64.com>
2018-02-15 11:45:15 +0200

    MOBI-1143 Add PV to mini-cart

--- src/Plugin/Checkout/CustomerData/Cart.php
+++ src/Plugin/Checkout/CustomerData/Cart.php
@@ -11,18 +11,23 @@ namespace Praxigento\Pv\Plugin\Checkout\CustomerData;
  */
 class Cart
 {
-    const CFG_PV_WRHS = 'prxgt_pv_wrhs';
+    const CFG_CAN_SEE_PV = 'prxgt_pv_can_see';
+    const CFG_ITEM_PV_WRHS = 'prxgt_pv_wrhs';
 
     /** @var \Praxigento\Pv\Api\Helper\GetPv */
     private $hlpGetPv;
+    /** @var \Praxigento\Pv\Helper\Customer */
+    private $hlpPvCust;
     /** @var \Praxigento\Pv\Repo\Entity\Stock\Item */
     private $repoPvStockItem;
 
     public function __construct(
         \Praxigento\Pv\Repo\Entity\Stock\Item $repoPvStockItem,
+        \Praxigento\Pv\Helper\Customer $hlpPvCust,
         \Praxigento\Pv\Api\Helper\GetPv $hlpGetPv
     ) {
         $this->repoPvStockItem = $repoPvStockItem;
+        $this->hlpPvCust = $hlpPvCust;
         $this->hlpGetPv = $hlpGetPv;
     }
 
@@ -30,11 +35,20 @@ class Cart
         \Magento\Checkout\CustomerData\Cart $subject,
         $result
     ) {
-        if ($result && isset($result['items']) && is_array($result['items'])) {
-            foreach ($result['items'] as $key => $item) {
-                $prodId = $item['product_id'];
-                $pvWrhs = $this->hlpGetPv->product($prodId);
-                $result['items'][$key][self::CFG_PV_WRHS] = $pvWrhs;
+        if (is_array($result)) {
+            $canSeePv = $this->hlpPvCust->canSeePv();
+            $result[self::CFG_CAN_SEE_PV] = $canSeePv;
+            if ($canSeePv) {
+                if (isset($result['items']) && is_array($result['items'])) {
+                    foreach ($result['items'] as $key => $item) {
+                        $prodId = $item['product_id'];
+                        $pvWrhs = $this->hlpGetPv->product($prodId);
+                        $result['items'][$key][self::CFG_ITEM_PV_WRHS] = $pvWrhs;
+                        /* this is not good idea, but "This is MAGENTA-A-A-A!!!!" */
+                        /* it is not a big price to have a 'canSeePv' flag for each item */
+                        $result['items'][$key][self::CFG_CAN_SEE_PV] = $canSeePv;
+                    }
+                }
             }
         }
         return $result;
