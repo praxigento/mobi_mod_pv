756757da0d8b964ac4f45ff6f9e0c412aa5bb94a
Alex Gusev <alex@flancer64.com>
2018-02-15 14:36:35 +0200

    MOBI-1143 Add PV to mini-cart

--- src/Plugin/Checkout/CustomerData/Cart.php
+++ src/Plugin/Checkout/CustomerData/Cart.php
@@ -6,29 +6,34 @@
 
 namespace Praxigento\Pv\Plugin\Checkout\CustomerData;
 
+use Praxigento\Pv\Config as Cfg;
+
 /**
  * Add warehouse PV to mini-cart items.
  */
 class Cart
 {
     const CFG_CAN_SEE_PV = 'prxgt_pv_can_see';
-    const CFG_ITEM_PV_WRHS = 'prxgt_pv_wrhs';
-
-    /** @var \Praxigento\Pv\Api\Helper\GetPv */
-    private $hlpGetPv;
+    const CFG_PV_TOTAL = 'prxgt_pv_total';
     /** @var \Praxigento\Pv\Helper\Customer */
     private $hlpPvCust;
-    /** @var \Praxigento\Pv\Repo\Entity\Stock\Item */
-    private $repoPvStockItem;
+    /** @var \Praxigento\Core\App\Repo\IGeneric */
+    private $repoGeneric;
+    /** @var \Praxigento\Pv\Repo\Entity\Quote */
+    private $repoQuote;
+    /** @var \Praxigento\Pv\Repo\Entity\Quote\Item */
+    private $repoQuoteItem;
 
     public function __construct(
-        \Praxigento\Pv\Repo\Entity\Stock\Item $repoPvStockItem,
-        \Praxigento\Pv\Helper\Customer $hlpPvCust,
-        \Praxigento\Pv\Api\Helper\GetPv $hlpGetPv
+        \Praxigento\Core\App\Repo\IGeneric $repoGeneric,
+        \Praxigento\Pv\Repo\Entity\Quote $repoQuote,
+        \Praxigento\Pv\Repo\Entity\Quote\Item $repoQuoteItem,
+        \Praxigento\Pv\Helper\Customer $hlpPvCust
     ) {
-        $this->repoPvStockItem = $repoPvStockItem;
+        $this->repoGeneric = $repoGeneric;
+        $this->repoQuote = $repoQuote;
+        $this->repoQuoteItem = $repoQuoteItem;
         $this->hlpPvCust = $hlpPvCust;
-        $this->hlpGetPv = $hlpGetPv;
     }
 
     public function afterGetSectionData(
@@ -41,16 +46,34 @@ class Cart
             if ($canSeePv) {
                 if (isset($result['items']) && is_array($result['items'])) {
                     foreach ($result['items'] as $key => $item) {
-                        $prodId = $item['product_id'];
-                        $pvWrhs = $this->hlpGetPv->product($prodId);
-                        $result['items'][$key][self::CFG_ITEM_PV_WRHS] = $pvWrhs;
+                        $itemId = $item['item_id'];
+                        $pvItem = $this->repoQuoteItem->getById($itemId);
+                        $totalItem = $pvItem->getTotal();
+                        $totalItem = number_format($totalItem, 2, '.', '');
+                        $result['items'][$key][self::CFG_PV_TOTAL] = $totalItem;
                         /* this is not good idea, but "This is MAGENTA-A-A-A!!!!" */
                         /* it is not a big price to have a 'canSeePv' flag for each item */
                         $result['items'][$key][self::CFG_CAN_SEE_PV] = $canSeePv;
                     }
+                    /* get total PV for quote itself */
+                    $quoteId = $this->getQuoteIdByItemId($itemId);
+                    $pvQuote = $this->repoQuote->getById($quoteId);
+                    $totalQuote = $pvQuote->getTotal();
+                    $totalQuote = number_format($totalQuote, 2, '.', '');
+                    $result[self::CFG_PV_TOTAL] = $totalQuote;
                 }
             }
         }
+        return $result;
+    }
+
+    private function getQuoteIdByItemId($itemId)
+    {
+        $tbl = Cfg::ENTITY_MAGE_QUOTE_ITEM;
+        $id = [Cfg::E_QUOTE_ITEM_A_ITEM_ID => $itemId];
+        $cols = [Cfg::E_QUOTE_ITEM_A_QUOTE_ID];
+        $rs = $this->repoGeneric->getEntityByPk($tbl, $id, $cols);
+        $result = reset($rs);
         return $result;
     }
 }
