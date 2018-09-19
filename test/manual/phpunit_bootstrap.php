73248c25106887e3fb7727ababc755fbf5a9e469
Alex Gusev <alex@flancer64.com>
2018-09-19 16:33:24 +0300

    SAN-339 Upgrade Magento up to 2.2.6

--- test/manual/phpunit_bootstrap.php
+++ test/manual/phpunit_bootstrap.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Start M2 app if BP is not defined (see './app/autoload.php' in Magento 2 root folder
+ * Include composer autoloader.
+ * Path is relative to "./work/vendor/<vendor>/<module>/test/manual/" folder.
  */
+include_once(__DIR__ . '/../../../../autoload.php');
+/* Start M2 app */
+/* BP is defined in Magento's ./app/autoload.php */
 if (!defined('BP')) {
     include_once(__DIR__ . '/../../../../../app/bootstrap.php');
     /**
-     * Create test application that loads configuration and  initializes DB connection
-     * then ends w/o exiting ($response->terminateOnSend = false).
+     * Create test application that initializes DB connection and ends w/o exiting
+     *  ($response->terminateOnSend = false).
      */
     $params = $_SERVER;
     /** @var  $bootstrap \Magento\Framework\App\Bootstrap */
