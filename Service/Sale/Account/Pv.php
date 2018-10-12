949ee4ccacdc10001b5f8c1cb531cfd04987c613
Alex Gusev <alex@flancer64.com>
2018-10-12 14:45:59 +0300

    SAN-389 Удвоение PV

--- Service/Sale/Account/Pv.php
+++ Service/Sale/Account/Pv.php
@@ -24,12 +24,15 @@ class Pv
     private $daoGeneric;
     /** @var  \Praxigento\Pv\Repo\Dao\Sale */
     private $daoSale;
+    /** @var \Psr\Log\LoggerInterface */
+    private $logger;
     /** @var  \Praxigento\Accounting\Api\Service\Account\Get */
     private $servAccount;
     /** @var \Praxigento\Accounting\Api\Service\Operation\Create */
     private $servOper;
 
     public function __construct(
+        \Praxigento\Core\Api\App\Logger\Main $logger,
         \Praxigento\Core\Api\App\Repo\Generic $daoGeneric,
         \Praxigento\Downline\Repo\Dao\Customer $daoDwnlCust,
         \Praxigento\Accounting\Api\Service\Account\Get $servAccount,
@@ -37,6 +40,7 @@ class Pv
         \Praxigento\Pv\Repo\Dao\Sale $daoSale
     )
     {
+        $this->logger = $logger;
         $this->daoGeneric = $daoGeneric;
         $this->daoDwnlCust = $daoDwnlCust;
         $this->daoSale = $daoSale;
@@ -55,42 +59,51 @@ class Pv
         $customerId = $request->getCustomerId();
         $dateApplied = $request->getDateApplied();
         $sale = $this->daoSale->getById($saleId);
-        $pvTotal = $sale->getTotal();
-        /* get customer for sale order */
-        list($saleCustId, $saleIncId) = $this->getSaleOrderData($saleId);
-        if (is_null($customerId)) {
-            $customerId = $saleCustId;
-        }
-        if (!is_null($customerId)) {
-            $mlmId = $this->getMlmId($customerId);
-            $note = "PV for sale #$mlmId";
-            /* get PV account data for customer */
-            $reqGetAccCust = new AAccountGetRequest();
-            $reqGetAccCust->setCustomerId($customerId);
-            $reqGetAccCust->setAssetTypeCode(Cfg::CODE_TYPE_ASSET_PV);
-            $respGetAccCust = $this->servAccount->exec($reqGetAccCust);
-            /* get PV account data for system */
-            $reqGetAccSys = new AAccountGetRequest();
-            $reqGetAccSys->setAssetTypeCode(Cfg::CODE_TYPE_ASSET_PV);
-            $reqGetAccSys->setIsSystem(TRUE);
-            $respGetAccSys = $this->servAccount->exec($reqGetAccSys);
-            /* create one operation with one transaction */
-            $reqAddOper = new AOperationRequest();
-            $reqAddOper->setOperationTypeCode(Cfg::CODE_TYPE_OPER_PV_SALE_PAID);
-            $reqAddOper->setOperationNote($note);
-            $trans = [
-                ATransaction::A_DEBIT_ACC_ID => $respGetAccSys->getId(),
-                ATransaction::A_CREDIT_ACC_ID => $respGetAccCust->getId(),
-                ATransaction::A_VALUE => $pvTotal,
-                ATransaction::A_DATE_APPLIED => $dateApplied,
-                ATransaction::A_NOTE => $note
-            ];
-            $reqAddOper->setTransactions([$trans]);
-            $respAddOper = $this->servOper->exec($reqAddOper);
-            $operId = $respAddOper->getOperationId();
-            $result->setOperationId($operId);
-            $result->markSucceed();
+        $tranId = $sale->getTransRef();
+        if (is_null($tranId)) {
+            $pvTotal = $sale->getTotal();
+            /* get customer for sale order */
+            list($saleCustId, $saleIncId) = $this->getSaleOrderData($saleId);
+            if (is_null($customerId)) {
+                $customerId = $saleCustId;
+            }
+            if (!is_null($customerId)) {
+                $mlmId = $this->getMlmId($customerId);
+                $note = "PV for sale #$mlmId";
+                /* get PV account data for customer */
+                $reqGetAccCust = new AAccountGetRequest();
+                $reqGetAccCust->setCustomerId($customerId);
+                $reqGetAccCust->setAssetTypeCode(Cfg::CODE_TYPE_ASSET_PV);
+                $respGetAccCust = $this->servAccount->exec($reqGetAccCust);
+                /* get PV account data for system */
+                $reqGetAccSys = new AAccountGetRequest();
+                $reqGetAccSys->setAssetTypeCode(Cfg::CODE_TYPE_ASSET_PV);
+                $reqGetAccSys->setIsSystem(TRUE);
+                $respGetAccSys = $this->servAccount->exec($reqGetAccSys);
+                /* create one operation with one transaction */
+                $reqAddOper = new AOperationRequest();
+                $reqAddOper->setOperationTypeCode(Cfg::CODE_TYPE_OPER_PV_SALE_PAID);
+                $reqAddOper->setOperationNote($note);
+                $trans = [
+                    ATransaction::A_DEBIT_ACC_ID => $respGetAccSys->getId(),
+                    ATransaction::A_CREDIT_ACC_ID => $respGetAccCust->getId(),
+                    ATransaction::A_VALUE => $pvTotal,
+                    ATransaction::A_DATE_APPLIED => $dateApplied,
+                    ATransaction::A_NOTE => $note
+                ];
+                $reqAddOper->setTransactions([$trans]);
+                $respAddOper = $this->servOper->exec($reqAddOper);
+                $operId = $respAddOper->getOperationId();
+                $tranIds = $respAddOper->getTransactionsIds();
+                $tranId = reset($tranIds);
+                $sale->setTransRef($tranId);
+                $this->daoSale->updateById($saleId, $sale);
+                $result->setOperationId($operId);
+                $result->markSucceed();
+            } else {
+            }
         } else {
+            $this->logger->error("PV accounting error: there is transaction #$tranId for sale #$saleId.");
         }
         return $result;
     }
