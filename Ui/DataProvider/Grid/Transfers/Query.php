f7d22289c2fee352de455356aab10bb8cdb63065
Alex Gusev <alex@flancer64.com>
2019-01-03 17:39:11 +0200

    SAN-499 Add columns to PV batch transfer

--- Ui/DataProvider/Grid/Transfers/Query.php
+++ Ui/DataProvider/Grid/Transfers/Query.php
@@ -16,6 +16,7 @@ class Query
 {
     /**#@+ Tables aliases for external usage ('camelCase' naming) */
     const AS_FROM = 'f';
+    const AS_GROUP = 'g';
     const AS_ITEMS = 'i';
     const AS_NAME_FROM = 'nf';
     const AS_NAME_TO = 'nt';
@@ -24,19 +25,24 @@ class Query
 
     /**#@+ Columns/expressions aliases for external usage */
     const A_BATCH_ID = 'batchId';
+    const A_FROM_COUNTRY = 'fromCountry';
     const A_FROM_ID = 'fromId';
     const A_FROM_MLM_ID = 'fromMlmId';
     const A_FROM_NAME = 'fromName';
     const A_ITEM_ID = 'itemId';
     const A_RESTRICTED = 'restricted';
+    const A_TO_COUNTRY = 'toCountry';
+    const A_TO_GROUP = 'toGroup';
     const A_TO_ID = 'toId';
     const A_TO_MLM_ID = 'toMlmId';
     const A_TO_NAME = 'toName';
+    const A_TO_PATH = 'toPath';
     const A_VALUE = 'value';
     /**#@- */
 
     /**#@+ Entities are used in the query */
     const E_FROM = EDwnlCust::ENTITY_NAME;
+    const E_GROUP = Cfg::ENTITY_MAGE_CUSTOMER_GROUP;
     const E_ITEMS = EItem::ENTITY_NAME;
     const E_NAME_FROM = Cfg::ENTITY_MAGE_CUSTOMER;
     const E_NAME_TO = Cfg::ENTITY_MAGE_CUSTOMER;
@@ -78,14 +84,18 @@ class Query
             $expFullNameTo = $this->expFullNameTo();
             $map = [
                 self::A_BATCH_ID => self::AS_ITEMS . '.' . EItem::A_BATCH_REF,
+                self::A_FROM_COUNTRY => self::AS_FROM . '.' . EDwnlCust::A_COUNTRY_CODE,
                 self::A_FROM_ID => self::AS_ITEMS . '.' . EItem::A_CUST_FROM_REF,
                 self::A_FROM_MLM_ID => self::AS_FROM . '.' . EDwnlCust::A_MLM_ID,
                 self::A_FROM_NAME => $expFullNameFrom,
                 self::A_ITEM_ID => self::AS_ITEMS . '.' . EItem::A_ID,
                 self::A_RESTRICTED => self::AS_ITEMS . '.' . EItem::A_RESTRICTED,
+                self::A_TO_COUNTRY => self::AS_TO . '.' . EDwnlCust::A_COUNTRY_CODE,
+                self::A_TO_GROUP => self::AS_GROUP . '.' . Cfg::E_CUSTGROUP_A_CODE,
                 self::A_TO_ID => self::AS_ITEMS . '.' . EItem::A_CUST_TO_REF,
                 self::A_TO_MLM_ID => self::AS_TO . '.' . EDwnlCust::A_MLM_ID,
                 self::A_TO_NAME => $expFullNameTo,
+                self::A_TO_PATH => self::AS_TO . '.' . EDwnlCust::A_PATH,
                 self::A_VALUE => self::AS_ITEMS . '.' . EItem::A_VALUE
             ];
             $this->mapper = new \Praxigento\Core\App\Repo\Query\Criteria\Def\Mapper($map);
@@ -99,27 +109,35 @@ class Query
      * `i`.`batch_ref` AS `batchId`,
      * `i`.`id` AS `itemId`,
      * `i`.`cust_from_ref` AS `fromId`,
-     * `i`.`cust_from_ref` AS `fromName`,
      * `i`.`cust_to_ref` AS `toId`,
-     * `i`.`cust_to_ref` AS `toName`,
      * `i`.`restricted`,
      * `i`.`value`,
+     * `f`.`country_code` AS `fromCountry`,
      * `f`.`mlm_id` AS `fromMlmId`,
-     * ( fn.firstname + " " + fn.lastname ) AS `fromName`,
+     * (CONCAT(nf.firstname,
+     * " ",
+     * nf.lastname)) AS `fromName`,
+     * `t`.`country_code` AS `toCountry`,
      * `t`.`mlm_id` AS `toMlmId`,
-     * ( tn.firstname + " " + tn.lastname ) AS `toName`
+     * `t`.`path` AS `toPath`,
+     * (CONCAT(nt.firstname,
+     * " ",
+     * nt.lastname)) AS `toName`,
+     * `g`.`customer_group_code` AS `toGroup`
      * FROM
      * `prxgt_pv_trans_batch_item` AS `i`
      * LEFT JOIN `prxgt_dwnl_customer` AS `f` ON
      * f.customer_ref = i.cust_from_ref
-     * LEFT JOIN `customer_entity` AS `fn` ON
-     * fn.entity_id = f.customer_ref
+     * LEFT JOIN `customer_entity` AS `nf` ON
+     * nf.entity_id = f.customer_ref
      * LEFT JOIN `prxgt_dwnl_customer` AS `t` ON
      * t.customer_ref = i.cust_to_ref
-     * LEFT JOIN `customer_entity` AS `tn` ON
-     * tn.entity_id = f.customer_ref
+     * LEFT JOIN `customer_entity` AS `nt` ON
+     * nt.entity_id = t.customer_ref
+     * LEFT JOIN `customer_group` AS `g` ON
+     * g.customer_group_id = nt.group_id
      * WHERE
-     * ( i.batch_ref = 0 )
+     * (i.batch_ref = :batchId)
      *
      */
     protected function getQueryItems()
@@ -129,6 +147,7 @@ class Query
         /* define tables aliases for internal usage (in this method) */
         $asItems = self::AS_ITEMS;
         $asFrom = self::AS_FROM;
+        $asGroup = self::AS_GROUP;
         $asFromName = self::AS_NAME_FROM;
         $asTo = self::AS_TO;
         $asToName = self::AS_NAME_TO;
@@ -150,6 +169,7 @@ class Query
         $tbl = $this->resource->getTableName(self::E_FROM);
         $as = $asFrom;
         $cols = [
+            self::A_FROM_COUNTRY => EDwnlCust::A_COUNTRY_CODE,
             self::A_FROM_MLM_ID => EDwnlCust::A_MLM_ID
         ];
         $cond = $as . '.' . EDwnlCust::A_CUSTOMER_REF . '=' . $asItems . '.' . EItem::A_CUST_FROM_REF;
@@ -169,7 +189,9 @@ class Query
         $tbl = $this->resource->getTableName(self::E_TO);
         $as = $asTo;
         $cols = [
-            self::A_TO_MLM_ID => EDwnlCust::A_MLM_ID
+            self::A_TO_COUNTRY => EDwnlCust::A_COUNTRY_CODE,
+            self::A_TO_MLM_ID => EDwnlCust::A_MLM_ID,
+            self::A_TO_PATH => EDwnlCust::A_PATH
         ];
         $cond = $as . '.' . EDwnlCust::A_CUSTOMER_REF . '=' . $asItems . '.' . EItem::A_CUST_TO_REF;
         $result->joinLeft([$as => $tbl], $cond, $cols);
@@ -182,6 +204,15 @@ class Query
             self::A_TO_NAME => $exp
         ];
         $cond = $as . '.' . Cfg::E_CUSTOMER_A_ENTITY_ID . '=' . $asTo . '.' . EDwnlCust::A_CUSTOMER_REF;
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+
+        /* LEFT JOIN customer_group (to) */
+        $tbl = $this->resource->getTableName(self::E_GROUP);
+        $as = $asGroup;
+        $cols = [
+            self::A_TO_GROUP => Cfg::E_CUSTGROUP_A_CODE
+        ];
+        $cond = $as . '.' . Cfg::E_CUSTGROUP_A_ID . '=' . $asToName . '.' . Cfg::E_CUSTOMER_A_GROUP_ID;
         $result->joinLeft([$as => $tbl], $cond, $cols);
 
         /**
