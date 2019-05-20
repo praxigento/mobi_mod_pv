dcc2f66571781e5c82b6c18fe13ad7630d0c9d3e
Alex Gusev <alex@flancer64.com>
2019-05-20 19:24:08 +0300

    SAN-649 PV value in sales order total info

--- Helper/Customer.php
+++ Helper/Customer.php
@@ -16,34 +16,57 @@ class Customer
     private $daoPvCustGroup;
     /** @var \Magento\Customer\Model\Session */
     private $session;
+    /** @var \Magento\Framework\App\State */
+    private $state;
 
     public function __construct(
+        \Magento\Framework\App\State $state,
         \Magento\Customer\Model\Session $session,
         \Praxigento\Pv\Repo\Dao\Customer\Group $daoPvCustGroup
     ) {
+        $this->state = $state;
         $this->session = $session;
         $this->daoPvCustGroup = $daoPvCustGroup;
     }
 
     /**
-     * Cached accessor for 'Can See PV' flag.
+     * Cached accessor for 'Can See PV' flag bound to customer's groups.
      *
      * @param int|null $gid customer group ID, if 'null' - group for current customer is used.
      * @return bool
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function canSeePv($gid = null)
     {
-        $result = false;
-        if (is_null($gid)) {
-            $gid = $this->session->getCustomerGroupId();
+        $result = $this->isAdmin();
+        if (!$result) {
+            if (is_null($gid)) {
+                $gid = $this->session->getCustomerGroupId();
+            }
+
+            if (!isset($this->cacheCanSeeByGid[$gid])) {
+                $item = $this->daoPvCustGroup->getById($gid);
+                if ($item) {
+                    $result = (bool)$item->getCanSeePv();
+                }
+                $this->cacheCanSeeByGid[$gid] = $result;
+            } else {
+                $result = $this->cacheCanSeeByGid[$gid];
+            }
         }
-        if (!isset($this->cacheCanSeeByGid[$gid])) {
-            $item = $this->daoPvCustGroup->getById($gid);
-            if ($item) $result = (bool)$item->getCanSeePv();
-            $this->cacheCanSeeByGid[$gid] = $result;
-        } else {
-            $result = $this->cacheCanSeeByGid[$gid];
-        }
+        return $result;
+    }
+
+    /**
+     * Return 'true' for adminhtml area (backend).
+     *
+     * @return bool
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function isAdmin()
+    {
+        $areaCode = $this->state->getAreaCode();
+        $result = ($areaCode == \Magento\Framework\App\Area::AREA_ADMINHTML);
         return $result;
     }
 }
